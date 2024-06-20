eb711fff756a2900dcf6cf731ed3d97527b9d7e7
Raffity23 <121945907+Raffity23@users.noreply.github.com>
2024-06-20 22:12:52 +0500

    Update BlastEffect.cs

--- BlastEffect.cs
+++ BlastEffect.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class BlastEffect : MonoBehaviour
 {
-    [SerializeField] private float _explosionRadius;
-    [SerializeField] private float _explosionForce;
-
-    public void Explode(Cube[] cubes)
+    [SerializeField] private float _defaultExplosionRadius = 20f;
+    [SerializeField] private float _defaultExplosionForce = 400f;
+    
+    public void Explode()
     {
-        foreach (Cube cube in cubes)
+        _defaultExplosionRadius = ChangeExplodeValue(_defaultExplosionRadius);
+        _defaultExplosionForce = ChangeExplodeValue(_defaultExplosionForce);
+
+        Collider[] objectsInRadius = Physics.OverlapSphere(transform.position, _defaultExplosionRadius);
+
+        foreach (Collider objectInRadius in objectsInRadius)
         {
-            cube.TryGetComponent<Rigidbody>(out Rigidbody cubeRigidbody);
-            cubeRigidbody.AddExplosionForce(_explosionForce, gameObject.transform.position, _explosionRadius);
+            if (objectInRadius.TryGetComponent<Rigidbody>(out Rigidbody cubeRigidbody))
+                cubeRigidbody.AddExplosionForce(_defaultExplosionForce, 
+                    gameObject.transform.position, _defaultExplosionRadius);
         }
+    }
+
+    private float ChangeExplodeValue(float value)
+    {
+        return value / transform.localScale.x;
     }
 }
